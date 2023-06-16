7be09c8c2a9847053d49e54d8371eb905d03dee7
mjcha <mjchambers83@gmail.com>
2023-06-15 21:44:35 -0400

    Add project files.

---
+++ REST_API/Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using REST_API.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace REST_API.Data {
+
+    public class ApiContext : DbContext {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) {  }
+    }
+}
---
+++ REST_API/Models/HotelBooking.cs
@@ -0,0 +1,8 @@
+﻿namespace REST_API.Models {
+
+    public class HotelBooking {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string? ClientName { get; set; }
+    }
+}
